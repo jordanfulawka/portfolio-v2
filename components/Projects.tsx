65d7c04e41b4168b50aaf4fce9ddb42fabc112a8
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-04-28 03:04:17 -0400

    fix: project card size adjustment

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
       <h1 className='text-2xl sm:text-3xl md:text-4xl font-mono mt-24 md:mt-32 lg:mt-40'>
         Here are some things I&apos;ve been working on lately
       </h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-8 pb-8'>
+      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-8 pb-8 justify-items-center'>
         {projects.map((project) => {
           return <Project key={project.title} project={project} />;
         })}
