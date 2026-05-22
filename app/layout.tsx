70dc9e1e9005f3ad05a30f4f88fe8dbf3a031f7e
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-05-22 08:00:01 -0400

    feat: add vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const jetbrainsMono = JetBrains_Mono({
   variable: '--font-jetbrains-mono',
@@ -18,11 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang='en'
-      className={`${jetbrainsMono.variable} h-full antialiased`}
-    >
-      <body className='min-h-full flex flex-col'>{children}</body>
+    <html lang='en' className={`${jetbrainsMono.variable} h-full antialiased`}>
+      <body className='min-h-full flex flex-col'>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
